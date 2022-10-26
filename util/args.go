5406edaf1f991af81de6d6ef442e71f59ee8b81d
Mike Chirico <mike.chirico@irhythmtech.com>
2022-10-26 18:04:46 -0400

    update-automated-save

--- util/args.go
+++ util/args.go
@@ -16,8 +16,15 @@ func Get(key string) (string, error) {
 }
 
 func GetAWS() ([]string, error) {
+	if _, err := Get("AWS_DEFAULT_REGION"); err != nil {
+		_ = os.Setenv("AWS_DEFAULT_REGION", "us-east-1")
+	}
+
 	out := []string{}
-	for _, key := range []string{"AWS_ACCESS_KEY_ID", "AWS_SECRET_ACCESS_KEY", "AWS_SESSION_TOKEN"} {
+	for _, key := range []string{"AWS_ACCESS_KEY_ID",
+		"AWS_SECRET_ACCESS_KEY",
+		"AWS_SESSION_TOKEN",
+		"AWS_DEFAULT_REGION"} {
 		env := os.Getenv(key)
 		if env == "" {
 			return nil, fmt.Errorf("key %s not set", key)
@@ -36,5 +43,7 @@ func ReadReplace(file string, out []string) (string, error) {
 	s := strings.Replace(string(dat), "${AWS_ACCESS_KEY_ID}", fmt.Sprintf("%q", out[0]), -1)
 	s = strings.Replace(s, "${AWS_SECRET_ACCESS_KEY}", fmt.Sprintf("%q", out[1]), -1)
 	s = strings.Replace(s, "${AWS_SESSION_TOKEN}", fmt.Sprintf("%q", out[2]), -1)
+	s = strings.Replace(s, "${AWS_DEFAULT_REGION}", fmt.Sprintf("%q", out[3]), -1)
+
 	return s, nil
 }
