4f8a0f123e3a4a1416ce4ec1351708b4cb0659bd
Mike Chirico <mike.chirico@irhythmtech.com>
2022-10-26 20:05:41 -0400

    update-automated-save

--- cmd/dev.go
+++ cmd/dev.go
@@ -10,9 +10,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// argoCmd represents the argo command
-var argoCmd = &cobra.Command{
-	Use:   "argo",
+// devCmd represents the dev command
+var devCmd = &cobra.Command{
+	Use:   "dev",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -21,7 +21,7 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		r, err := fixtures.Read("../fixtures/argo/argo.yaml")
+		r, err := fixtures.Read("../fixtures/argo/argoUbunto.yaml")
 		if err != nil {
 			fmt.Println(err)
 			return
@@ -31,15 +31,15 @@ to quickly create a Cobra application.`,
 }
 
 func init() {
-	rootCmd.AddCommand(argoCmd)
+	rootCmd.AddCommand(devCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// argoCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// devCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// argoCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// devCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
