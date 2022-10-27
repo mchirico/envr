0e21d6519f2226a6ddeaf21fc04498b9c2521dc6
Mike Chirico <mike.chirico@irhythmtech.com>
2022-10-27 10:01:45 -0400

    update-automated-save

---
+++ cmd/pod.go
@@ -0,0 +1,45 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"github.com/mchirico/envr/fixtures"
+
+	"github.com/spf13/cobra"
+)
+
+// podCmd represents the pod command
+var podCmd = &cobra.Command{
+	Use:   "pod",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		r, err := fixtures.Read("../fixtures/pod/pod.yaml")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(string(r))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(podCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// podCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// podCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
