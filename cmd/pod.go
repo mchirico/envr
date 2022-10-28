2d9ddde4c7849edbb6a6eb024ed226b65e60312b
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2022-10-28 08:49:29 -0400

    update-automated-save

--- cmd/pod.go
+++ cmd/pod.go
@@ -13,13 +13,8 @@ import (
 // podCmd represents the pod command
 var podCmd = &cobra.Command{
 	Use:   "pod",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "dispalys pod template",
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		r, err := fixtures.Read("../fixtures/pod/pod.yaml")
 		if err != nil {
