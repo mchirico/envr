a1f11b93383d5137ad05ec4493e5d3b2c3ed09e6
Mike Chirico (https://mchirico.github.io/) <mchirico@gmail.com>
2022-11-03 22:18:52 -0400

    update-automated-save

--- cmd/version.go
+++ cmd/version.go
@@ -17,7 +17,7 @@ var versionCmd = &cobra.Command{
 
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("\n\n version 0.0.15\n\n")
+		fmt.Println("\n\n version 0.0.16\n\n")
 	},
 }
 
